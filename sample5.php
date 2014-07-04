b065b1cc1fe46d9a98ee3b0668105b480ee76467
snk kny <shinkikenya@mkbka143.kyoto-su.ac.jp>
2014-07-04 16:23:30 +0900

    edited

--- sample5.php
+++ sample5.php
@@ -1,15 +1,18 @@
-<!DOCTYPE html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
   <head>
-  <link rel="stylesheet" href="sample5.css" type="text/css">
+<link rel="stylesheet" href="css/sample5.css" type="text/css">
+<script src="js/jquery-1.11.0.min.js"></script>
+<script src="js/lightbox.min.js"></script>
+<link href="css/lightbox.css" rel="stylesheet" />
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>集合写真検索システム</title>
   </head>
   <body>
 <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">  
    <p>    
-   検索キーワード：<input type="text" name="keyword" size=20 /><br>
-   写真中の人の数：<input type="text" name="number" size=20  placeholder="1~3人までね"/><br>
+   検索キーワード：<input type="text" name="keyword" size=20/><br>
+   写真中の人の数：<input type="text" name="number" size=20 /><br>
    <input type="submit" value="Search!" />  
    </p>
 </form>
@@ -46,53 +49,59 @@ fclose($f2);
 
 // 以下、検索処理
 $result_num = 0;
+$number_han = mb_convert_kana($_POST["number"], 'ran', "UTF-8");
+$num;
+$search_result; //[][0:ファイル名 1:キーワード頻度 2:人数]
 
-if (isset($_POST["keyword"]) && isset($_POST["number"])) {  
+if (isset($_POST["keyword"]) && isset($number_han)) {  
   if(array_key_exists($_POST["keyword"], $tf_data) && $_POST["keyword"] <>null ){
     
-	if ($_POST["number"]==null || !preg_match("/^[0-9]+$/", $_POST["number"])) {
-		echo "人数を正しく入力して下さい。";
-	} else {
-    	echo "キーワード「".$_POST["keyword"]."」　人数「";
-    	echo $_POST["number"]."人」での検索結果<br>\n";
+    if($number_han==null){
+      echo "キーワード「".$_POST["keyword"]."」　人数「指定なし」での検索結果<br>\n";
+      $num = -1;
+    }elseif (!preg_match("/^[0-9]+$/", $number_han)) {
+      echo "人数を正しく入力して下さい。";
+    } else {
+      echo "キーワード「".$_POST["keyword"]."」　人数「";
+      echo $number_han."人」での検索結果<br>\n";
+      $num = 0;
     }
     echo "<hr><br>\n";
-    
+    $search_result = array( array() );
     foreach($tf_data[@$_POST["keyword"]] as $key => $val ) {
-      if (@$_POST["number"] == @$fc_data[$key] && @$_POST["number"]<>null){
-		echo "<img src='$key'><br>\n";
-		echo "キーワード出現回数＝".$val."回<br>\n";
-		echo "写真中の人の数＝".@$fc_data[$key]."人<br>\n";
-		echo "$key<br><br><br>\n";
-		$result_num++;
+      if ((@$number_han == @$fc_data[$key] && @$number_han!=null) || $num == -1){
+        // $search_result[$result_num] = array(){$key, $val, $fc_data[$key]};
+        $search_result[$result_num][0] = $key;
+        $search_result[$result_num][1] = $val;
+        $search_result[$result_num][2] = $fc_data[$key];
+        if(isset($search_result[2])) {
+          $search_result[$result_num][2] = 0;
+        }
+    // echo "<img src='$key'><br>\n";
+    // echo "キーワード出現回数＝".$val."回<br>\n";
+    // echo "写真中の人の数＝".@$fc_data[$key]."人<br>\n";
+    // echo "$key<br><br><br>\n";
+        $result_num++;
       }
     }
-    
   } elseif (@$_POST["keyword"]==null) {
     echo '検索キーワードを入力して下さい。';
   } else {
-    echo '検索キーワードに合致する写真はありません。';
+    echo '検索キーワード「'.@$_POST["keyword"].'」に合致する写真はありません。';
   }
 }
 
+if(isset($search_result)) {
+  foreach ($search_result as $value) {
+	  echo "<a href='$value[0]' rel='lightbox'><img src='$value[0]'></a><br>\n";
+      echo "キーワード出現回数＝".$value[1]."回<br>\n";
+      echo "写真中の人の数＝".$value[2]."人<br>\n";
+      echo "$key<br><br><br>\n";
+  }
+}
+if(isset($_POST["keyword"]))
 echo "検索結果は".$result_num."件でした。";
 
 ?>
 </body>
 </html>
-=======
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
-<html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>集合写真検索システム</title>
-  </head>
-  <body>
-<form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">  
-   <p>    
-   検索キーワード：<input type="text" name="keyword" size=20/><br>
-   写真中の人の数：<input type="text" name="number" size=20 /><br>
-   <input type="submit" value="Search!" />  
-   </p>
-</form>
-
